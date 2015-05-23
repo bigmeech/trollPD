4d33ecb6830c71ec7a563ee267f0eda53660593d
Eliemenye, Larry <Larry.Eliemenye@tuitravel.com>
2015-05-23 08:45:14 +0100

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+gulp.task('default', function(){
+    console.log('this is a default task running')
+});
+
+gulp.task('build-server', function(){
+    console.log('this is a task for building stuff');
+});
